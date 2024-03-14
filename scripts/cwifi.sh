f514f1fc2ddf9a3158c80a8c7874f8e0ed85f2bd
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-14 20:10:54 +0800

    更新 cwifi.sh

--- scripts/cwifi.sh
+++ scripts/cwifi.sh
@@ -19,7 +19,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 sed -i "s/hostname='OpenWrt'/hostname='QihooV6'/g" package/base-files/files/bin/config_generate
 
 # Add a feed source
-echo 'src-git small-package https://github.com/kenzok8/small-package;main' >>feeds.conf.default
+echo 'src-git small_package https://github.com/kenzok8/small-package;main' >>feeds.conf.default
 # echo 'src-git openwrt_packages https://github.com/wwz09/openwrt-packages;master' >>feeds.conf.default
 # echo 'src-git openwrt_small https://github.com/wwz09/small;master' >>feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5' >>feeds.conf.default

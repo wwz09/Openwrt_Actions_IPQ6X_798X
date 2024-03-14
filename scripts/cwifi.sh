40e8de2cd3cc4e196e8ab64c927a7911147b2dfc
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-14 22:10:32 +0800

    更新 cwifi.sh

--- scripts/cwifi.sh
+++ scripts/cwifi.sh
@@ -5,7 +5,13 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 #　修改主机名
 sed -i "s/hostname='OpenWrt'/hostname='QihooV6'/g" package/base-files/files/bin/config_generate
+# 删除luci所在行
+# sed -i '/luci/d' feeds.conf.default
 
+# 删除所有空白行
+# sed －i '/^\s*$/d' feeds.conf.default
+
+# echo 'src-git luci https://github.com/immortalwrt/luci.git' >>feeds.conf.default
 echo 'src-git small_package https://github.com/kenzok8/small-package.git;main' >>feeds.conf.default
 # echo 'src-git openwrt_packages https://github.com/wwz09/openwrt-packages;master' >>feeds.conf.default
 echo 'src-git openwrt_small https://github.com/wwz09/small;master' >>feeds.conf.default

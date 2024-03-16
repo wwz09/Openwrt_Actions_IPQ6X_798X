dd74a2363c00dd9b9535e77e119668872d5d8287
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-16 15:04:10 +0800

    sh

--- scripts/WIFI2.sh
+++ scripts/WIFI2.sh
@@ -35,6 +35,7 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 rm -rf feeds/luci/applications
 git clone  https://github.com/wwz09/applications feeds/luci/applications
 git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
+git clone https://github.com/rianjskis/luci-app-filetransfer.git luci/applications/luci-app-filetransfer
 git clone  https://github.com/sirpdboy/luci-app-parentcontrol.git feeds/luci/applications/luci-app-parentcontrol
 # git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
 

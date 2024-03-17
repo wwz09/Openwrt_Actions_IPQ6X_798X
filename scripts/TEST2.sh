ce9d778e37a837f51faa3f76f551dc65be35f4a5
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-17 19:59:00 +0800

    更新 TEST2.sh

--- scripts/TEST2.sh
+++ scripts/TEST2.sh
@@ -46,7 +46,7 @@ git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpu
 # git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 #　编译的固件文件名
-sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=TEST-$(VERSION_DIST_SANITIZED)/g' include/image.mk
+sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=TEST测试版-$(VERSION_DIST_SANITIZED)/g' include/image.mk
 
 # 取消主题默认设置
 # find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;

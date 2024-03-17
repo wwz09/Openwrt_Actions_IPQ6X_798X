6e7220a00a454a72d78f4c31c310e7380c3e80fc
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-17 16:54:40 +0800

    sh

--- scripts/TEST1.sh
+++ scripts/TEST1.sh
@@ -26,7 +26,7 @@ sed －i '/^\s*$/d' feeds.conf.default
 echo 'src-git luci https://github.com/immortalwrt/luci.git' >>feeds.conf.default
 echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
 # echo 'src-git small_package https://github.com/kenzok8/small-package.git;main' >>feeds.conf.default
-# echo 'src-git openwrt_packages https://github.com/wwz09/openwrt-packages;master' >>feeds.conf.default
+echo 'src-git openwrt_packages https://github.com/wwz09/openwrt-packages;master' >>feeds.conf.default
 echo 'src-git openwrt_small https://github.com/wwz09/small;master' >>feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5' >>feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default

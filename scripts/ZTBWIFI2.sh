e316b6a6d4c86bf6f933c61309bedf09c9fd6d9b
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-19 20:08:27 +0800

    sh

--- scripts/ZTBWIFI2.sh
+++ scripts/ZTBWIFI2.sh
@@ -13,9 +13,9 @@
 
 
 # 移除要替换的包
-rm -rf package/feeds/packages/uspot
-rm -rf package/feeds/sup/upx
-rm -rf package/feeds/sup/shadowsocksr-libev
+# rm -rf package/feeds/packages/uspot
+# rm -rf package/feeds/sup/upx
+# rm -rf package/feeds/sup/shadowsocksr-libev
 # rm -rf package/feeds/luci/luci-app-appfilter
 # rm -rf package/feeds/luci/luci-app-bitsrunlogin-go
 # rm -rf package/feeds/luci/luci-app-cpulimit
@@ -33,9 +33,9 @@ rm -rf package/feeds/sup/shadowsocksr-libev
 # 移除要替换的包
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-rm -rf feeds/luci/applications
-git clone https://github.com/wwz09/applications feeds/luci/applications
-# git clone  https://github.com/wwz09/sup.git feeds/luci/applications/sup
+# rm -rf feeds/luci/applications
+# git clone https://github.com/wwz09/applications feeds/luci/applications
+git clone  https://github.com/wwz09/sup.git feeds/luci/applications/sup
 git clone https://github.com/sirpdboy/luci-app-parentcontrol.git feeds/luci/applications/luci-app-parentcontrol
 # git clone https://github.com/rianjskis/luci-app-filetransfer.git luci/applications/luci-app-filetransfer
 git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan

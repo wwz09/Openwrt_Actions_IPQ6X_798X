cdde115a28f8ff3f57e651405de57fd4e78f8d5f
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-19 19:11:32 +0800

    sh

--- scripts/ZTBWIFIC.sh
+++ scripts/ZTBWIFIC.sh
@@ -32,7 +32,7 @@ echo 'src-git packages https://github.com/Lienol/openwrt-packages.git;22.03' >>f
 # echo 'src-git openwrt_small https://github.com/wwz09/small;master' >>feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5' >>feeds.conf.default
 echo 'src-git sup https://github.com/wwz09/sup.git' >>feeds.conf.default
-# echo 'src-git small-package https://github.com/kenzok8/small-package' >>feeds.conf.default
+# echo 'src-git small_package https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a

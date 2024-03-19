6d47c30b1c93784d16b45ad11d8fe73e43cd8477
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-20 07:19:32 +0800

    更新 ZTBWIFI2.sh

--- scripts/ZTBWIFI2.sh
+++ scripts/ZTBWIFI2.sh
@@ -36,10 +36,11 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 # rm -rf feeds/luci/applications
 # git clone https://github.com/wwz09/applications feeds/luci/applications
 # git clone  https://github.com/wwz09/sup.git feeds/luci/applications/sup
-# git clone https://github.com/sirpdboy/luci-app-parentcontrol.git feeds/luci/applications/luci-app-parentcontrol
-# git clone https://github.com/rianjskis/luci-app-filetransfer.git luci/applications/luci-app-filetransfer
-git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-# git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
+git clone https://github.com/sirpdboy/luci-app-parentcontrol.git luci/applications/luci-app-parentcontrol
+git clone https://github.com/rianjskis/luci-app-filetransfer.git luci/applications/luci-app-filetransfer
+git clone https://github.com/siwind/luci-app-usb_printer.git luci/applications/luci-app-usb_printer
+git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
+git clone  https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
 
 
 # 添加主题
@@ -48,6 +49,12 @@ git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpu
 
 #　编译的固件文件名
 sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=ZTBWIFI-$(VERSION_DIST_SANITIZED)/g' include/image.mk
+
+# 修复 hostapd 报错
+cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
+
+# 修改本地时间格式
+sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
 # 取消主题默认设置
 # find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;

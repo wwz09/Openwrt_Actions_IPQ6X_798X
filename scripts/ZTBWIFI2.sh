2d5b4dc7c7dee5d354ecedb88d4dd3279f58a9c6
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-03-20 07:27:13 +0800

    更新 ZTBWIFI2.sh

--- scripts/ZTBWIFI2.sh
+++ scripts/ZTBWIFI2.sh
@@ -54,7 +54,7 @@ sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=ZTBWIFI-$(VERSION_DI
 cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
 
 # 修改本地时间格式
-sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+# sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
 # 取消主题默认设置
 # find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;

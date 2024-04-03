c6b515a28f0c411d4fef23083e524cc029638a8a
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-04-03 16:34:16 +0800

    更新 ZYWIFI.sh

--- scripts/ZYWIFI.sh
+++ scripts/ZYWIFI.sh
@@ -45,7 +45,7 @@ git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebro
 git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-git_sparse_clone master  https://github.com/wwz09/luci_small luci-base  luci-compat luci
+git_sparse_clone master  https://github.com/wwz09/luci_small luci-base  luci-compat luci vlmcsd
 git_sparse_clone main https://github.com/wwz09/luci_app luci-app-p910nd luci-app-usb-printer luci-app-vlmcsd  luci-app-wolplus  luci-app-frpc  luci-app-control-webrestriction
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-control-weburl luci-app-control-timewol luci-app-firewall  luci-app-opkg luci-app-samba4 
 git clone  https://github.com/sirpdboy/luci-app-parentcontrol.git feeds/luci/applications/luci-app-parentcontrol

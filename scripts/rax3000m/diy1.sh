029b1103ed0b55d7e3c91387664ea152c050dfb4
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-04-28 19:53:56 +0800

    rax3000M

---
+++ scripts/rax3000m/diy1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#=================================================
+# MZwrt script
+#================================================= 
+
+
+##添加自己的插件库
+echo -e "\nsrc-git extraipk https://github.com/mzwrt/extra_ipk" >> feeds.conf.default

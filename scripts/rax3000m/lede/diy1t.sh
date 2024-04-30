2458158d5ccef264f44dcd60f6358fb7c1a8f105
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-04-30 21:48:09 +0800

    sh

---
+++ scripts/rax3000m/lede/diy1t.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#=================================================
+# MZwrt script
+#================================================= 
+
+
+##添加自己的插件库
+echo -e "\nsrc-git extraipk https://github.com/wwz09/RAX3000MIPK" >> feeds.conf.default

bfac05975f8d8d877b42bd4c67f85b1bffbc8718
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-05-01 17:06:28 +0800

    更新 diy09.sh

--- scripts/rax3000m/Imm/diy09.sh
+++ scripts/rax3000m/Imm/diy09.sh
@@ -11,5 +11,5 @@ sed -i '/packages/d' feeds.conf.default
 
 ##添加自己的插件库
 echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git luci packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
 echo -e "\nsrc-git extraipk https://github.com/mzwrt/extra_ipk" >> feeds.conf.default

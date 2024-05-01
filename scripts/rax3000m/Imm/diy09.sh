8054f4df981416f63351b8252c66334067a5f877
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-05-01 17:57:58 +0800

    更新 diy09.sh

--- scripts/rax3000m/Imm/diy09.sh
+++ scripts/rax3000m/Imm/diy09.sh
@@ -5,11 +5,11 @@
 
 
 # 删除luci所在行
-sed -i '/luci/d' feeds.conf.default
-sed -i '/packages/d' feeds.conf.default
+# sed -i '/luci/d' feeds.conf.default
+# sed -i '/packages/d' feeds.conf.default
 # sed -i '/small/d' feeds.conf.default
 
 ##添加自己的插件库
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git coolsnowwolf_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git coolsnowwolf_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
 echo -e "\nsrc-git extraipk https://github.com/mzwrt/extra_ipk" >> feeds.conf.default

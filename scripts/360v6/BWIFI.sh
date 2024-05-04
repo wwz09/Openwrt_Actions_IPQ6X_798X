eb300be4bfe770102391d6def435fd232f173355
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-05-04 20:45:48 +0800

    更新 BWIFI.sh

--- scripts/360v6/BWIFI.sh
+++ scripts/360v6/BWIFI.sh
@@ -22,6 +22,8 @@ sed -i "s/hostname='OpenWrt'/hostname='QihooV6'/g" package/base-files/files/bin/
 # 删除luci所在行
 # sed -i '/luci/d' feeds.conf.default
 # sed -i '/packages/d' feeds.conf.default
+# 删除空行
+# sed -i '/^$/d' feeds.conf.default
 sed -i '/small/d' feeds.conf.default
 
 # 删除所有空白行

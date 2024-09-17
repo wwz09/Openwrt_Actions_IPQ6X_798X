b610e27d28d18222a58191b2593250ea8e51a7c5
wwz09 <wwz5124@gmail.com>
2024-09-17 20:39:42 +0800

    del

--- scripts/rax3000m/istore/diy1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#=================================================
-# MZwrt script
-#================================================= 
-
-
-##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/wwz09/RAX3000MIPK" >> feeds.conf.default

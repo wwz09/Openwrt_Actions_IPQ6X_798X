b058f33b1e57138b881cbd01b1be4c7e37ea0e80
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-12-28 09:01:44 +0800

    sh

--- scripts/rax3000m/lede/diy1.sh
+++ scripts/rax3000m/lede/diy1.sh
@@ -6,3 +6,4 @@
 
 ##添加自己的插件库
 echo -e "\nsrc-git extraipk https://github.com/wwz09/RAX3000MIPK" >> feeds.conf.default
+# echo -e "\nsrc-git immo https://github.com/immortalwrt/luci.git;openwrt-21.02" >> feeds.conf.default

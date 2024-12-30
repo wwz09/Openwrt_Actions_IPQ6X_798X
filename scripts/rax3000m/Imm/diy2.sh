e4b5b9e4c83308b74151bb399d8dde99a92e2d12
wwz09 <47264059+wwz09@users.noreply.github.com>
2024-12-30 20:59:38 +0800

    更新 diy2.sh

--- scripts/rax3000m/Imm/diy2.sh
+++ scripts/rax3000m/Imm/diy2.sh
@@ -8,25 +8,6 @@
 ##配置IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-##
-rm -rf ./feeds/extraipk/theme/luci-theme-argon-18.06
-rm -rf ./feeds/extraipk/theme/luci-app-argon-config-18.06
-rm -rf ./feeds/extraipk/theme/luci-theme-design
-rm -rf ./feeds/extraipk/theme/luci-theme-ifit
-rm -rf ./feeds/extraipk/theme/luci-theme-opentopd
-rm -rf ./feeds/extraipk/theme/luci-theme-neobird
-
-rm -rf ./package/feeds/extraipk/luci-theme-argon-18.06
-rm -rf ./package/feeds/extraipk/luci-app-argon-config-18.06
-rm -rf ./package/feeds/extraipk/theme/luci-theme-design
-rm -rf ./package/feeds/extraipk/theme/luci-theme-ifit
-rm -rf ./package/feeds/extraipk/theme/luci-theme-opentopd
-rm -rf ./package/feeds/extraipk/theme/luci-theme-neobird
-
-rm -rf ./feeds/luci/luci-theme-argon
-rm -rf ./feeds/luci/luci-theme-openwrt
-rm -rf ./feeds/luci/luci-theme-openwrt-2020
-rm -rf ./feeds/luci/luci-theme-bootstrap-mod
 
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap

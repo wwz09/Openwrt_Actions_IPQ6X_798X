54d9675500d4c903789e048d28792bb832dfc66a
wwz09 <47264059+wwz09@users.noreply.github.com>
2025-03-14 13:30:08 +0800

    Update test1.sh

--- scripts/rax3000m/lede/test1.sh
+++ scripts/rax3000m/lede/test1.sh
@@ -5,9 +5,9 @@
 
 
 ##添加自己的插件库
-echo -e "\nsrc-git extraipk https://github.com/wwz09/RAX3000MIPK" >> feeds.conf.default
+# echo -e "\nsrc-git extraipk https://github.com/wwz09/RAX3000MIPK" >> feeds.conf.default
 # echo -e "\nsrc-git immo https://github.com/immortalwrt/luci.git;openwrt-21.02" >> feeds.conf.default
-# echo -e "\nsrc-git mzwrt_package https://github.com/mzwrt/mzwrt_package_Lite.git" >> feeds.conf.default
+ echo -e "\nsrc-git mzwrt_package https://github.com/mzwrt/mzwrt_package_Lite.git" >> feeds.conf.default
 
 
 # Git稀疏克隆，只克隆指定目录到本地

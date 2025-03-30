d84d6f4e7f869b966425b7de0e02bde56d2fb86f
wwz09 <47264059+wwz09@users.noreply.github.com>
2025-03-30 18:34:41 +0800

    del

--- scripts/rax3000m/Imm/diy3.sh
+++ scripts/rax3000m/Imm/diy3.sh
@@ -6,7 +6,7 @@
 
 
 ##配置IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 
 ##取消bootstrap为默认主题
@@ -29,14 +29,15 @@ sed -i.bak "s,mirrors.vsean.net/openwrt,mirrors.vsean.net/openwrt,g" package/emo
 
 
 ##WiFi
-sed -i "s/MT7981_AX3000_2.4G/BM520-2.4G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
-sed -i "s/MT7981_AX3000_5G/BM520-5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
+sed -i "s/MT7981_AX3000_2.4G/CM520-2.4G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
+sed -i "s/MT7981_AX3000_5G/CM520-5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
 
 ##New WiFi
-sed -i "s/ImmortalWrt-2.4G/BM520-2.4G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
-sed -i "s/ImmortalWrt-5G/BM520-5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
-
+sed -i "s/ImmortalWrt-2.4G/CM520-2.4G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-5G/CM520-5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 # 加入OpenClash核心
 chmod -R a+x $GITHUB_WORKSPACE/scripts/rax3000m/Imm/preset-clash-core.sh
 $GITHUB_WORKSPACE/scripts/rax3000m/Imm/preset-clash-core.sh
+
+

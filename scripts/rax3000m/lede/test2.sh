b9901235dbd12ed958f3a08e37fc8d471dd9b745
wwz09 <47264059+wwz09@users.noreply.github.com>
2025-04-05 21:33:12 +0800

    sh

--- scripts/rax3000m/lede/test2.sh
+++ scripts/rax3000m/lede/test2.sh
@@ -13,7 +13,7 @@
 ./scripts/feeds clean
 
 ##配置IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 ##
 rm -rf ./feeds/extraipk/theme/luci-theme-argon-18.06
@@ -34,6 +34,29 @@ rm -rf ./package/feeds/extraipk/theme/luci-theme-neobird
 
 
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+
+## 添加额外插件
+
+git_sparse_clone main https://github.com/Lienol/openwrt-package  luci-app-control-webrestriction 
+# git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-parentcontrol
+git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-timecontrol
+git_sparse_clone main https://github.com/sirpdboy/sirpdboy-package luci-app-control-timewol
+git_sparse_clone main https://github.com/ksong008/sirpdboy-package luci-app-control-weburl
+# git_sparse_clone main https://github.com/linkease/nas-packages-luci luci
+# git_sparse_clone main https://github.com/wwz09/RAX3000MIPK luci-app-parentcontrol
+# git clone  https://github.com/sirpdboy/luci-app-parentcontrol.git feeds/luci/applications/luci-app-parentcontrol
+# git clone  https://github.com/firkerword/luci-app-parentcontrol.git ./package/luci-app-parentcontrol
+
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/luci-theme-bootstrap/luci-theme-argone/g' feeds/luci/collections/luci/Makefile
@@ -44,10 +67,14 @@ sed -i 's/\/bin\/login/\/bin\/login -f root/' feeds/packages/utils/ttyd/files/tt
 
 
 #修改默认无线名称
-sed -i 's/LEDE/BM520-2.4G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/LEDE/TEST520-2.4G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #修改无线加密及密码
 sed -i 's/encryption=none/encryption=psk-mixed+ccmp\n            set wireless.default_radio${devidx}.key=abc5124937,\n/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 ##更改主机名
 sed -i "s/hostname='.*'/hostname='RAX3000M'/g" package/base-files/files/bin/config_generate
+
+# 加入OpenClash核心
+# chmod -R a+x $GITHUB_WORKSPACE/scripts/rax3000m/Imm/preset-clash-core.sh
+# $GITHUB_WORKSPACE/scripts/rax3000m/Imm/preset-clash-core.sh

1273f6f0fd91036917482e67fc70cbd1d145c6c4
wwz09 <47264059+wwz09@users.noreply.github.com>
2025-04-17 07:41:15 +0800

    Update test.sh

--- scripts/qihoo360v6/test.sh
+++ scripts/qihoo360v6/test.sh
@@ -9,3 +9,4 @@
 # echo -e "\nsrc-git extraipk https://github.com/wwz09/RAX3000MIPK" >> feeds.conf.default
 # echo -e "\nsrc-git immo https://github.com/immortalwrt/luci.git;openwrt-21.02" >> feeds.conf.default
 # echo -e "\nsrc-git mzwrt_package https://github.com/mzwrt/mzwrt_package_Lite.git" >> feeds.conf.default
+sed -i 's/luci.git/luci.git;openwrt-21.02/g' feeds.conf.default

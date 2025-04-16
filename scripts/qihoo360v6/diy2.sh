39d5ec2bd6572a76dd529f27b860e2c8688cda75
wwz09 <47264059+wwz09@users.noreply.github.com>
2025-04-16 23:39:16 +0800

    Update diy2.sh

--- scripts/qihoo360v6/diy2.sh
+++ scripts/qihoo360v6/diy2.sh
@@ -10,56 +10,21 @@
 # See /LICENSE for more information.
 #
 
-./scripts/feeds clean
+
 
 ##配置IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-##
-rm -rf ./feeds/extraipk/theme/luci-theme-argon-18.06
-rm -rf ./feeds/extraipk/theme/luci-app-argon-config-18.06
-rm -rf ./feeds/extraipk/theme/luci-theme-design
-rm -rf ./feeds/extraipk/theme/luci-theme-edge
-rm -rf ./feeds/extraipk/theme/luci-theme-ifit
-rm -rf ./feeds/extraipk/theme/luci-theme-opentopd
-rm -rf ./feeds/extraipk/theme/luci-theme-neobird
+# 移除要替换的包
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/msd_lite
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-netgear
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-serverchan
 
-
-rm -rf ./package/feeds/extraipk/luci-theme-argon-18.06
-rm -rf ./package/feeds/extraipk/luci-app-argon-config-18.06
-rm -rf ./package/feeds/extraipk/theme/luci-theme-design
-rm -rf ./package/feeds/extraipk/theme/luci-theme-edge
-rm -rf ./package/feeds/extraipk/theme/luci-theme-ifit
-rm -rf ./package/feeds/extraipk/theme/luci-theme-opentopd
-rm -rf ./package/feeds/extraipk/theme/luci-theme-neobird
-rm -rf ./feeds/packages/utils/coremark
-
-
-# rm -rf ./feeds/mzwrt_package/luci-app-shadowsocks
-# rm -rf ./feeds/mzwrt_package/luci-app-bypass
-# rm -rf ./feeds/mzwrt_package/luci-app-bandwidthd
-# rm -rf ./feeds/mzwrt_package/luci-app-ssr-plus
-# rm -rf ./feeds/mzwrt_package/luci-app-gowebdav
-
-
-#mkdir -p new
-#git clone --depth=1  https://github.com/mzwrt/mzwrt_package_Lite.git new/mzwrt
-#rm -rf new/mzwrt/luci-app-shadowsocks
-#rm -rf new/mzwrt/luci-app-bypass
-#rm -rf new/mzwrt/luci-app-bandwidthd
-#rm -rf new/mzwrt/luci-app-ssr-plus
-#rm -rf new/mzwrt/luci-app-gowebdav
-#rm -rf new/mzwrt/luci-app-ddnsto
-#rm -rf new/mzwrt/ddnsto
-#rm -rf new/mzwrt/dockerd
-#rm -rf new/mzwrt/luci-app-dockerman
-#rm -rf new/mzwrt/luci-app-smartdns
-#rm -rf new/mzwrt/smartdns
-#rm -rf new/mzwrt/luci-lib-taskd
-#rm -rf new/mzwrt/coremark
-
-#cp -rf new/mzwrt package/mzwrt
-#rm -rf new
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -74,18 +39,62 @@ function git_sparse_clone() {
 
 ## 添加额外插件
 
-git_sparse_clone main https://github.com/wwz09/mzwrt_package_Lite  luci-app-quickstart luci-app-store luci-app-adguardhome luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl luci-app-filebrowser luci-app-ikoolproxy luci-app-linkease luci-app-socat luci-app-vlmcsd luci-app-smartdns luci-theme-design luci-app-eqos 
-git_sparse_clone main https://github.com/wwz09/mzwrt_package_Lite  quickstart adguardhome filebrowser linkease smartdns lua-maxminddb ucl upx linkmount luci-lib-taskd taskd luci-lib-xterm coremark
-# git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-parentcontrol 
-# git_sparse_clone main https://github.com/linkease/nas-packages-luci luci
-# git_sparse_clone main https://github.com/chenmozhijin/luci-app-socat luci-app-socat
-# git clone  https://github.com/sirpdboy/luci-app-parentcontrol.git feeds/luci/applications/luci-app-parentcontrol
+git_sparse_clone main https://github.com/wwz09/mzwrt_package_Lite  luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl luci-app-filebrowser  luci-app-socat luci-app-vlmcsd   
+git_sparse_clone main https://github.com/wwz09/mzwrt_package_Lite   filebrowser 
+# 添加额外插件
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
+git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+# git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
+git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
+# git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
+
+# 科学上网插件
+#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git clone --depth=1 https://github.com/littletao12/openwrt-bin package/openwrt-bin
+git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/OpenWrt-nikki
+# Themes
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
+
+# 晶晨宝盒
+git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
+sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/haiibo/OpenWrt'|g" package/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|ARMv8|ARMv8_PLUS|g" package/luci-app-amlogic/root/etc/config/amlogic
+
+# SmartDNS
 git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+
+# msd_lite
+git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
+git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
+
+# MosDNS
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+
+# Alist
+git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
+
+# DDNS.to
 git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
 git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
-# git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-# git_sparse_clone main https://github.com/linkease/istore luci
+
+# iStore
+git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
+git_sparse_clone main https://github.com/linkease/istore luci
 
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap

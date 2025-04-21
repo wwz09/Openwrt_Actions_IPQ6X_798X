5f50a37ffd5607f09411da352864a053a27ce0d9
wwz09 <wwz5124@gmail.com>
2025-04-21 21:28:59 +0800

    Update test.sh

--- scripts/360v6/test.sh
+++ scripts/360v6/test.sh
@@ -23,5 +23,5 @@ echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >>
 # echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5" >> "feeds.conf.default"
 # echo "src-git openclash https://github.com/vernesong/OpenClash.git;dev" >> "feeds.conf.default"
  sed -i 's/luci.git/luci.git;openwrt-21.02/g' feeds.conf.default
- sed -i 's/packages.git/packages.git;openwrt-21.02/g' feeds.conf.default
+ sed -i 's|https://github.com/immortalwrt/packages.git|https://github.com/immortalwrt/packages.git;openwrt-21.02|g' feeds.conf.default
  

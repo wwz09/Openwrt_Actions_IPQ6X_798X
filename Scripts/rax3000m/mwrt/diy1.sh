bc6f2a7fbfbe3f20a42dfe8f6ca8be2563ccb874
wwz09 <wwz5124@gmail.com>
2025-04-23 21:18:42 +0800

    wrt

---
+++ Scripts/rax3000m/mwrt/diy1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#=================================================
+# MZwrt script
+#================================================= 
+
+
+##添加自己的插件库
+# echo 'src-git coolsnowwolf_luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+# echo 'src-git coolsnowwolf_packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+# echo -e "\nsrc-git extraipk https://github.com/wwz09/RAX3000MIPK" >> feeds.conf.default
+echo -e "\nsrc-git mzwrt_package https://github.com/mzwrt/mzwrt_package_Lite.git" >> feeds.conf.default
+# sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
+# sed -i '$a src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
---
+++ Scripts/rax3000m/mwrt/imm.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#=================================================
+# MZwrt script
+#=================================================             
+
+
+
+##配置IP
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+
+
+##取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+
+##更改主机名
+sed -i "s/hostname='.*'/hostname='RAX3000M'/g" package/base-files/files/bin/config_generate
+
+##加入作者信息
+sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='wwz09-$(date +%Y%m%d)'/g"  package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_REVISION='*.*'/DISTRIB_REVISION=' By Mz'/g" package/base-files/files/etc/openwrt_release
+# cp -af feeds/extraipk/patch/diy/banner-MZwrt  package/base-files/files/etc/banner
+
+# sed -i "2iuci set istore.istore.channel='MZ_wrt'" package/emortal/default-settings/files/99-default-settings
+sed -i "2iuci set istore.istore.channel='wwz09'" package/emortal/default-settings/files/99-default-settings
+sed -i "3iuci commit istore" package/emortal/default-settings/files/99-default-settings
+sed -i.bak "s,mirrors.vsean.net/openwrt,mirrors.vsean.net/openwrt,g" package/emortal/default-settings/files/99-default-settings
+
+
+##WiFi
+sed -i "s/MT7981_AX3000_2.4G/BM520-2.4G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
+sed -i "s/MT7981_AX3000_5G/BM520-5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
+
+##New WiFi
+sed -i "s/ImmortalWrt-2.4G/BM520-2.4G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-5G/BM520-5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+## golang编译环境
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+## 添加额外插件
+
+git_sparse_clone main https://github.com/mzwrt/mzwrt_package_Lite  luci-app-ikoolproxy luci-app-store luci-app-quickstart luci-app-openclash luci-app-easymesh luci-app-ddnsto luci-app-vlmcsd luci-theme-argon luci-theme-design luci-app-design-config luci-app-argon-config luci-app-lucky luci-app-smartdns luci-lib-xterm luci-lib-taskd luci-lib-iform
+
+git_sparse_clone main https://github.com/mzwrt/mzwrt_package_Lite  quickstart ucl upx taskd ddnsto filebrowser lua-maxminddb lucky smartdns upx-static docker
+
+# 新建new目录
+mkdir -p package/new
+
+## 加入 luci-app-socat
+rm -rf feeds/packages/net/socat
+git clone https://github.com/immortalwrt/packages package/new/immortalwrt-packages
+mv package/new/immortalwrt-packages/net/socat package/new/socat
+rm -rf package/new/immortalwrt-packages
+rm -rf feeds/luci/applications/luci-app-socat
+git clone --depth 1 https://github.com/chenmozhijin/luci-app-socat package/new/chenmozhijin-socat
+mv -n package/new/chenmozhijin-socat/luci-app-socat package/new/
+rm -rf package/new/chenmozhijin-socat
+
+## adguardhome
+#git clone -b patch-1 https://github.com/kiddin9/openwrt-adguardhome package/new/openwrt-adguardhome
+#mv package/new/openwrt-adguardhome/*adguardhome package/new/
+#rm -rf package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/AdGuardHome_template.yaml
+#cp -rf $GITHUB_WORKSPACE/patches/AdGuardHome/AdGuardHome_template.yaml #package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/AdGuardHome_template.yaml
+#rm -rf package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
+#cp -rf $GITHUB_WORKSPACE/patches/AdGuardHome/links.txt package/new/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
+# sed -i 's/+adguardhome/+PACKAGE_$(PKG_NAME)_INCLUDE_binary:adguardhome/g' package/new/luci-app-adguardhome/Makefile
+#rm -rf package/new/openwrt-adguardhome
+
+# 加入OpenClash核心
+chmod -R a+x $GITHUB_WORKSPACE/scripts/rax3000m/Imm/preset-clash-core.sh
+$GITHUB_WORKSPACE/scripts/rax3000m/Imm/preset-clash-core.sh
+
+

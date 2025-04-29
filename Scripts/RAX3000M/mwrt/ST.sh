ed9069ef299e3e0a4680b0d0cb904f161c9e7283
wwz09 <wwz5124@gmail.com>
2025-04-30 07:05:12 +0800

    sh

--- Scripts/RAX3000M/mwrt/ST.sh
+++ Scripts/RAX3000M/mwrt/ST.sh
@@ -6,7 +6,7 @@
 
 
 ##配置IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 
 #修改默认主题
